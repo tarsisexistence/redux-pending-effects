e5aef72ffff16d8e93d6ac5a872e26d2bf060717
maktarsis <tarsis.maksym@gmail.com>
2020-05-11 12:54:35 +0300

    chore: apply indepedency

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,8 @@
-const path = require('path');
-
 module.exports = {
-  extends: path.resolve(__dirname, '../../.eslintrc.js'),
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 6,
-    sourceType: 'module',
+    sourceType: "module",
     ecmaFeatures: {
       modules: true
     }
