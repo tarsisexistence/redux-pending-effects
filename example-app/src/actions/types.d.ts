45891f5762dd0d60849f5d7ff62a0e9749171da9
Denys Goloborodko <den.od.1911@gmail.com>
2020-07-19 21:04:51 +0300

    Added types for patents actions types

--- example-app/src/actions/types.d.ts
+++ example-app/src/actions/types.d.ts
@@ -1,20 +1,27 @@
 declare namespace Actions {
+  enum PATENTS_ACTION_TYPES {
+    GET = 'GET_PATENTS',
+    PENDING = 'GET_PATENTS_PENDING',
+    FULFILLED = 'GET_PATENTS_FULFILLED',
+    REJECTED = 'GET_PATENTS_REJECTED'
+  }
+
   interface IGetPatents {
-    type: string,
+    type: ReturnType<typeof PATENTS_ACTION_TYPES.GET>,
     payload: Promise<Patents.PatentDataShape[] | { status: string }>
   }
 
   interface IGetPatentsPending {
-    type: string,
+    type:  ReturnType<typeof PATENTS_ACTION_TYPES.PENDING>,
   }
 
   interface IGetPatentsFulFilled {
-    type: string,
+    type:  ReturnType<typeof PATENTS_ACTION_TYPES.FULFILLED>,
     payload: Patents.PatentDataShape[]
   }
 
   interface IGetPatentsRejected {
-    type: string,
+    type:  ReturnType<typeof PATENTS_ACTION_TYPES.REJECTED>,
     payload: { status: string }
   }
 
