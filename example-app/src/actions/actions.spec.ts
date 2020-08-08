3a1ebc6ca5cce3b188dd08c06d8a450e1fdbb023
retarsis <tarsis.maksym@gmail.com>
2020-08-08 22:11:10 +0300

    refactor: tests

--- example-app/src/actions/actions.spec.ts
+++ example-app/src/actions/actions.spec.ts
@@ -2,7 +2,7 @@ import configureStore from 'redux-mock-store';
 import { getDefaultMiddleware } from '@reduxjs/toolkit';
 import { promiseMiddleware, toolkitMiddleware } from 'redux-pending-effects';
 import createSagaMiddleware from '@redux-saga/core';
-import fetchMock from 'jest-fetch-mock';
+import fetchMock, { MockParams } from 'jest-fetch-mock';
 
 import { patentsActionsNames } from '../constants';
 import { getPatents } from './index';
@@ -20,9 +20,15 @@ const mockStore = configureStore(middleware);
 
 describe('testing async actions', () => {
   const apiKey = 'WmyhwhhQBZJIvTdIQ6KeYZUNenQY7Fazyd2nauB5';
+  let store: ReturnType<typeof mockStore>;
+
+  beforeEach(() => {
+    store = mockStore({});
+  });
 
   afterEach(() => {
-    fetchMock.resetMocks()
+    fetchMock.resetMocks();
+    store = null as any;
   });
 
   it('creates GET_PATENTS_FULFILLED when fetching patents has been done', () => {
@@ -33,32 +39,34 @@ describe('testing async actions', () => {
           '5bcafde29600021147742e3a',
           'LEW-TOPS-125',
           'Aircraft Engine Icing Event Avoidance and Mitigation',
-          'Innovators at NASA\'s Glenn Research Center',
+          "Innovators at NASA's Glenn Research Center",
           'LEW-TOPS-125',
           'aeronautics',
           '',
           '',
           '',
           '',
-          'https://technology.nasa.gov/test.jpg',
+          'https://technology.nasa.gov/test.jpg'
         ]
-      }
+      } as MockParams
     );
 
     const expectedActions = [
       { type: patentsActionsNames.PENDING },
-      { type: patentsActionsNames.FULFILLED, payload: [{
-        id: '5bcafde29600021147742e3a',
-        title: 'Aircraft Engine Icing Event Avoidance and Mitigation',
-        description: 'Innovators at NASA\'s Glenn Research Center',
-        imageUrl:'https://technology.nasa.gov/test.jpg'
-      }]}
+      {
+        type: patentsActionsNames.FULFILLED,
+        payload: [
+          {
+            id: '5bcafde29600021147742e3a',
+            title: 'Aircraft Engine Icing Event Avoidance and Mitigation',
+            description: "Innovators at NASA's Glenn Research Center",
+            imageUrl: 'https://technology.nasa.gov/test.jpg'
+          }
+        ]
+      }
     ];
 
-    const store = mockStore({});
-
     store.dispatch(getPatents());
-
-    return expect(store.getActions().toEqual(expectedActions))
-  })
+    expect(store.getActions()).toEqual(expectedActions);
+  });
 });
