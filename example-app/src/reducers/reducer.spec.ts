d0007270a86545c613eefb29a35916794f492646
dengoloborodko <den.od.1911@gmail.com>
2020-08-18 16:08:32 +0300

    test: Refactor tests
    1) Move store initialization from separate test cases to beforeEach
    2) Add correct processing of getLibraryContent fetch

--- example-app/src/reducers/reducer.spec.ts
+++ example-app/src/reducers/reducer.spec.ts
@@ -1,3 +1,4 @@
+import { Store } from 'redux';
 import { selectIsPending } from 'redux-pending-effects';
 import SagaTester from 'redux-saga-tester';
 import { configureStore } from '@reduxjs/toolkit';
@@ -5,31 +6,27 @@ import { configureStore } from '@reduxjs/toolkit';
 import { middleware, sagaMiddleware } from '../store';
 import { getPatents, getLibraryContent, getAstronomyPictureData } from '../actions';
 import { astronomyPictureActionNames } from '../constants';
-import { nasaService } from '../services/NasaService';
 import { astronomyPictureWorker } from '../sagas/astronomyPictureSagas';
 import { rootReducer, rootReducer as reducer } from './rootReducer';
 import { rootSaga } from '../sagas';
 
-const createStore = () => {
-  return configureStore({ reducer, middleware });
-};
-
 describe('selector', () => {
-  it('should have default negative pending state', () => {
-    const store = createStore();
+  let store: Store;
 
+  beforeEach(() => {
+    store = configureStore({ reducer, middleware });
+  });
+
+  it('should have default negative pending state', () => {
     expect(selectIsPending(store.getState())).toBe(false);
   });
 
   it('should trigger pending state after getPatents fetch started', async () => {
-    const store = createStore();
-
     store.dispatch(getPatents());
     expect(selectIsPending(store.getState())).toBe(true);
   });
 
   it('should complete pending state after getPatents fetch completed', async () => {
-    const store = createStore();
     const getPatentsAction: Actions.GetPatents = getPatents();
 
     store.dispatch(getPatentsAction);
@@ -38,30 +35,22 @@ describe('selector', () => {
   });
 
   it('should trigger pending state after getLibraryContent fetch started', async () => {
-    const store = createStore();
-
     store.dispatch<any>(getLibraryContent('test'));
     expect(selectIsPending(store.getState())).toBe(true);
   });
 
   it('should complete pending state after getLibraryContent fetch completed', async () => {
-    const store = createStore();
-
-    store.dispatch<any>(getLibraryContent('test'));
-    await nasaService.getLibraryContent('test');
+    await store.dispatch<any>(getLibraryContent('test'));
     expect(selectIsPending(store.getState())).toBe(false);
   });
 
   it('should trigger pending state after getAstronomyPictureData fetch started', async () => {
-    const store = createStore();
-
     sagaMiddleware.run(rootSaga);
     store.dispatch(getAstronomyPictureData);
     expect(selectIsPending(store.getState())).toBe(true);
   });
 
   it('should complete pending state after getAstronomyPictureData fetch completed', async () => {
-    const store = createStore();
     const sagaTester = new SagaTester({
       initialState: undefined,
       reducers: rootReducer
@@ -74,14 +63,12 @@ describe('selector', () => {
   });
 
   it('should not complete pending state after one of fetches is completed', async () => {
-    const store = createStore();
     const getPatentsAction: Actions.GetPatents = getPatents();
 
     store.dispatch(getPatentsAction);
-    store.dispatch<any>(getLibraryContent('test'));
 
     await Promise.race([
-      nasaService.getLibraryContent('test'),
+      store.dispatch<any>(getLibraryContent('test')),
       getPatentsAction.payload
     ]);
 
@@ -89,14 +76,12 @@ describe('selector', () => {
   });
 
   it('should complete pending state after all fetches are completed', async () => {
-    const store = createStore();
     const getPatentsAction: Actions.GetPatents = getPatents();
 
     store.dispatch(getPatentsAction);
-    store.dispatch<any>(getLibraryContent('test'));
 
     await Promise.all([
-      nasaService.getLibraryContent('test'),
+      store.dispatch<any>(getLibraryContent('test')),
       getPatentsAction.payload
     ]);
 
