3bb69852169345287587005546e08d8ec4c9b769
dengoloborodko <den.od.1911@gmail.com>
2020-08-18 08:03:20 +0300

    test: Added tests for selectIsPending state with several fetches at once

--- example-app/src/reducers/reducer.spec.ts
+++ example-app/src/reducers/reducer.spec.ts
@@ -23,12 +23,15 @@ describe('selector', () => {
     expect(selectIsPending(store.getState())).toBe(false);
   });
 
-  it('should trigger pending state on getPatents fetch started', async () => {
-    store.dispatch(getPatents());
+  it('should trigger pending state after   fetch started',async () => {
+    const getPatentsAction: Actions.GetPatents = getPatents();
+
+    store.dispatch(getPatentsAction);
     expect(selectIsPending(store.getState())).toBe(true);
+    await getPatentsAction.payload;
   });
 
-  it('should complete pending state on getPatents fetch completed', async () => {
+  it('should complete pending state after getPatents fetch completed', async () => {
     const getPatentsAction: Actions.GetPatents = getPatents();
 
     store.dispatch(getPatentsAction);
@@ -36,23 +39,31 @@ describe('selector', () => {
     expect(selectIsPending(store.getState())).toBe(false);
   });
 
-  it('should trigger pending state on getLibraryContent fetch started', async () => {
+  it('should trigger pending state after getLibraryContent fetch started', async () => {
     store.dispatch<any>(getLibraryContent('test'));
     expect(selectIsPending(store.getState())).toBe(true);
+    await nasaService.getLibraryContent('test');
   });
 
-  it('should complete pending state on getLibraryContent fetch completed', async () => {
+  it('should complete pending state after getLibraryContent fetch completed', async () => {
     store.dispatch<any>(getLibraryContent('test'));
     await nasaService.getLibraryContent('test');
     expect(selectIsPending(store.getState())).toBe(false);
   });
 
-  it('should trigger pending state on getAstronomyPictureData fetch started', async () => {
+  it('should trigger pending state after getAstronomyPictureData fetch started', async () => {
+    const sagaTester = new SagaTester({
+      initialState: undefined,
+      reducers: rootReducer
+    });
+
+    sagaTester.start(astronomyPictureWorker);
     store.dispatch(getAstronomyPictureData);
     expect(selectIsPending(store.getState())).toBe(true);
+    await sagaTester.waitFor(astronomyPictureActionNames.FULFILLED);
   });
 
-  it('should complete pending state on getAstronomyPictureData fetch completed', async () => {
+  it('should complete pending state after getAstronomyPictureData fetch completed', async () => {
     const sagaTester = new SagaTester({
       initialState: undefined,
       reducers: rootReducer
@@ -62,5 +73,33 @@ describe('selector', () => {
     store.dispatch(getAstronomyPictureData);
     await sagaTester.waitFor(astronomyPictureActionNames.FULFILLED);
     expect(selectIsPending(store.getState())).toBe(false);
+  });
+
+  it('should not complete pending state after one of fetches is completed', async () => {
+    const getPatentsAction: Actions.GetPatents = getPatents();
+
+    store.dispatch(getPatentsAction);
+    store.dispatch<any>(getLibraryContent('test'));
+
+    await Promise.race([
+      nasaService.getLibraryContent('test'),
+      getPatentsAction.payload
+    ]);
+
+    expect(selectIsPending(store.getState())).toBe(true)
+  });
+
+  it('should complete pending state after all fetches are completed', async () => {
+    const getPatentsAction: Actions.GetPatents = getPatents();
+
+    store.dispatch(getPatentsAction);
+    store.dispatch<any>(getLibraryContent('test'));
+
+    await Promise.all([
+      nasaService.getLibraryContent('test'),
+      getPatentsAction.payload
+    ]);
+
+    expect(selectIsPending(store.getState())).toBe(false)
   });
 });
