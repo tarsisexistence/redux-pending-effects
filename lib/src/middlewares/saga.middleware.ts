78cc3dcb54f329bc909e666d6ec8055211927db5
dengoloborodko <den.od.1911@gmail.com>
2020-09-14 08:33:49 +0300

    feat: Add pendingSagaMiddleware implementation

--- lib/src/middlewares/saga.middleware.ts
+++ lib/src/middlewares/saga.middleware.ts
@@ -1,14 +1,12 @@
 import { AnyAction } from 'redux';
-import { put } from '@redux-saga/core/effects';
+import { EffectMiddleware } from '@redux-saga/core';
+import { put, effectTypes as sagaEffectTypes } from '@redux-saga/core/effects';
 
 import { patchEffect } from '../store/actions';
 import { nanoid } from '../helpers/nanoid.utils';
 import { effectTypes } from '../helpers/const';
 
-// not implemented
-// export const pendingSagaMiddleware =  null;
-
-export const trackWorker = <T = any>(
+const trackWorker = <T = any>(
   worker: (action: AnyAction) => any
 ): ((action: AnyAction) => Generator<any>) =>
   function* wrapper(action: AnyAction) {
@@ -29,3 +27,16 @@ export const trackWorker = <T = any>(
       yield put(patchEffect(patchEffectPayload));
     }
   };
+
+export const pendingSagaMiddleware: EffectMiddleware = next => effect => {
+  if (effect.type === sagaEffectTypes.FORK) {
+    const effectArgs = effect.payload.args;
+    const sagaWorkerArgIndex = 1;
+
+    effectArgs[sagaWorkerArgIndex] = trackWorker(
+      effectArgs[sagaWorkerArgIndex]
+    );
+  }
+
+  return next(effect);
+};
